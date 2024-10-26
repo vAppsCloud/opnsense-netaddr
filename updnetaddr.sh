6ce77906c644c08ac3eebe7f8bce99088be256dc
Parag Mehta <pm@vappscloud.com>
2024-10-25 22:09:53 -0700

    Update updnetaddr.sh
    modified and fixed to use the latest python version running in opnsense

--- updnetaddr.sh
+++ updnetaddr.sh
@@ -1,3 +1,5 @@
+#!/usr/local/bin/bash
+
 # Copyright 2023-2024 Parag Mehta (06/15/2023)
 #
 # GNU General Public License version 3 or later.
@@ -9,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/bash
+ver=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))')
 
 # Filename variables to overwrite
 # netaddr/eui
@@ -21,17 +23,41 @@ FNIPv6="ipv6-address-space.xml"
 FNMCAST="multicast-addresses.xml"
 FNv6UCAST="ipv6-unicast-address-assignments.xml"
 
-# sources of the file locations are taken from netaddr Makefile
-# https://github.com/netaddr/netaddr/blob/master/Makefile
-# download: section
+# Backup function with date-timestamp if file exists
+backup_if_exists() {
+    local filepath=$1
+    if [ -f "$filepath" ]; then
+        local timestamp=$(date +%Y%m%d_%H%M%S)
+        mv "$filepath" "${filepath}_backup_$timestamp"
+        echo "Backup created for $filepath as ${filepath}_backup_$timestamp"
+    fi
+}
 
-# get oui and iab files from ieee.org site
-wget -qO /usr/local/lib/python3.9/site-packages/netaddr/eui/$FNOUI http://standards-oui.ieee.org/oui/$FNOUI
-wget -qO /usr/local/lib/python3.9/site-packages/netaddr/eui/$FNIAB http://standards-oui.ieee.org/oui/$FNIAB
-# generate indices
-python3 /usr/local/lib/python3.9/site-packages/netaddr/eui/ieee.py
-# get iana ip files in xml format
-wget -qO /usr/local/lib/python3.9/site-packages/netaddr/ip/$FNIPv4 https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.xml
-wget -qO /usr/local/lib/python3.9/site-packages/netaddr/ip/$FNIPv6 https://www.iana.org/assignments/ipv6-address-space/ipv6-address-space.xml
-wget -qO /usr/local/lib/python3.9/site-packages/netaddr/ip/$FNMCAST https://www.iana.org/assignments/multicast-addresses/multicast-addresses.xml
-wget -qO /usr/local/lib/python3.9/site-packages/netaddr/ip/$FNv6UCAST https://www.iana.org/assignments/ipv6-unicast-address-assignments/ipv6-unicast-address-assignments.xml
+# Sources of the file locations are taken from netaddr Makefile
+# https://github.com/netaddr/netaddr/blob/master/Makefile
+
+# Download and backup files
+base_path="/usr/local/lib/python$ver/site-packages/netaddr"
+
+# Get oui and iab files from ieee.org site
+backup_if_exists "$base_path/eui/$FNOUI"
+curl -s -o "$base_path/eui/$FNOUI" https://standards-oui.ieee.org/oui/$FNOUI
+
+backup_if_exists "$base_path/eui/$FNIAB"
+curl -s -o "$base_path/eui/$FNIAB" https://standards-oui.ieee.org/iab/$FNIAB
+
+# Generate indices
+python3 /usr/local/lib/python$ver/site-packages/netaddr/eui/ieee.py
+
+# Get iana ip files in xml format
+backup_if_exists "$base_path/ip/$FNIPv4"
+curl -s -o "$base_path/ip/$FNIPv4" https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.xml
+
+backup_if_exists "$base_path/ip/$FNIPv6"
+curl -s -o "$base_path/ip/$FNIPv6" https://www.iana.org/assignments/ipv6-address-space/ipv6-address-space.xml
+
+backup_if_exists "$base_path/ip/$FNMCAST"
+curl -s -o "$base_path/ip/$FNMCAST" https://www.iana.org/assignments/multicast-addresses/multicast-addresses.xml
+
+backup_if_exists "$base_path/ip/$FNv6UCAST"
+curl -s -o "$base_path/ip/$FNv6UCAST" https://www.iana.org/assignments/ipv6-unicast-address-assignments/ipv6-unicast-address-assignments.xml
